9437392b39872fda2aaef808ff81054584c72578
IsacSmile <94504780+IsacSmile@users.noreply.github.com>
2026-08-24 21:38:45 +0530

    copy: update copyright notice to © 2026 Thrine Media - Built with Intent.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -111,7 +111,7 @@ export const Footer: React.FC = () => {
             </button>
           </div>
           <div>
-            © 2026 Thrine Studio. All client work 100% owned by clients.
+            © 2026 Thrine Media - Built with Intent.
           </div>
         </div>
       </Container>
